cf661221be5381a1d2d2e921ced2a7d6b747c3b7
agzuniverse <aswinganesh666@gmail.com>
2017-12-29 00:42:15 +0530

    Final updates

--- Tabscreen.js
+++ Tabscreen.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import {Image, Text, TouchableNativeFeedback, TouchableHighlight} from 'react-native';
 import {TabNavigator} from 'react-navigation';
-import {Container, Header, Icon, Left, Body, Thumbnail, Fab} from 'native-base';
+import {Container, Header, Icon, Left, Body, Thumbnail, Fab, Form, Item, Input} from 'native-base';
 import Timeline from './Timeline';
 
 const Mainscreen=TabNavigator(
   {
       Main:{
-        screen:Timeline,
+        screen: Timeline,
         navigationOptions:{
           tabBarIcon: ({tintColor}) => <Icon name='home' style={{color:tintColor}}/>
         }
       },
       Search:{
-        screen: props => <Container style={{backgroundColor:'rgba(0,132,180,0.5)'}}></Container>,
+        screen: props => <Container style={{backgroundColor:'rgba(0,132,180,0.5)'}}><Form><Item rounded bordered style={{backgroundColor:'rgba(255,255,255,0.87)', marginLeft:10, marginRight:10, marginTop:10}}><Input placeholder="Search..."/></Item></Form></Container>,
         navigationOptions:{
           tabBarIcon: ({tintColor}) => <Icon name='search' style={{color:tintColor}}/>
         }
@@ -51,19 +51,12 @@ const Mainscreen=TabNavigator(
 );
 
 class Tabscreen extends React.Component {
-  constructor(){
-    super();
-    this.handler = this.handler.bind(this)
+  constructor(props){
+    super(props);
     this.state={
-      header:<Text style={{color:'rgba(0,0,0,0.87)', fontWeight:'700', fontSize:16}}>{'\n'}  Home</Text>
+      header:<Text style={{color:'rgba(0,0,0,0.87)', fontWeight:'700', fontSize:16}}>{'\n'}  Home</Text>,
+      globalStateUpdate: newState => this.setState(newState)
     };
-  }
-
-  handler(e) {
-    e.preventDefault()
-    this.setState({
-      //someVar: someValue
-    })
   }
 
   render() {
@@ -71,7 +64,7 @@ class Tabscreen extends React.Component {
       <Container>
         <Header hasTabs style={{height:90, backgroundColor:'rgba(255,255,255,0.87)'}} >
           <Left style={{flexDirection:'row'}}>
-          <TouchableHighlight onPress={()=>this.props.navigation.navigate('DrawerOpen')} underlayColor='black'>
+          <TouchableHighlight onPress={()=>this.props.navigation.navigate('DrawerOpen')} underlayColor='grey'>
             <Thumbnail source={{uri:'https://avatars1.githubusercontent.com/u/22353313?s=460&v=4'}} />
           </TouchableHighlight>
             {this.state.header}
@@ -83,7 +76,7 @@ class Tabscreen extends React.Component {
             <Icon name='ios-paper-plane'/>
           </Fab>
         </TouchableNativeFeedback>
-        <Mainscreen handler={this.handler}/>
+        <Mainscreen globalStateUpdate={this.state.globalStateUpdate}/>
       </Container>
     );
   }
--- Timeline.js
+++ Timeline.js
@@ -3,7 +3,7 @@ import {Image, Text} from 'react-native';
 import {Container, Header, Content, Card, CardItem, Thumbnail, Button, Icon, Left, Body} from 'native-base';
 import {Col, Row, Grid} from 'react-native-easy-grid';
 
-class Timeline extends React.Component {
+export default class Timeline extends React.Component {
   render() {
     return (
       <Container>
@@ -58,22 +58,22 @@ class Timeline extends React.Component {
         <Card style={{flex: 0}}>
           <CardItem>
             <Left>
-              <Thumbnail source={{uri:'https://avatars1.githubusercontent.com/u/22353313?s=460&v=4'}} />
+              <Thumbnail source={{uri:'https://pbs.twimg.com/profile_images/890111871633813506/snFDZkX0_400x400.jpg'}} />
               <Body>
                 <Text style={{fontWeight:'700'}}>
-                  Aswin G
-                  <Text style={{color:'rgba(0,0,0,0.3)'}}>  @aswing1999</Text>
+                  Jesus Christ
+                  <Text style={{color:'rgba(0,0,0,0.3)'}}>  @thegoodgodabove</Text>
                 </Text>
-                <Text note style={{fontWeight:'100'}}>December 18, 2017</Text>
+                <Text note style={{fontWeight:'100'}}>December 25, 2017</Text>
               </Body>
             </Left>
           </CardItem>
           <CardItem>
             <Body>
             <Text>
-                Have a great day everyone!{'\n'}{'\n'}
+                It's my birthday!{'\n'}{'\n'}
             </Text>
-              <Image source={{uri: 'https://c1.staticflickr.com/9/8475/8448486325_dc7eb0fcaf_n.jpg'}} style={{height: 150, alignSelf: 'stretch', flex: 1, resizeMode:'stretch'}}/>
+              <Image source={{uri: 'http://s3.india.com/wp-content/uploads/2017/12/Teaser-7-1.jpg'}} style={{height: 150, alignSelf: 'stretch', flex: 1, resizeMode:'stretch'}}/>
             </Body>
           </CardItem>
           <CardItem>
@@ -82,15 +82,15 @@ class Timeline extends React.Component {
                 <Col size={4}></Col>
                 <Col size={24} style={{flexDirection:'row'}}>
                   <Icon name="ios-text-outline"/>
-                  <Text style={{color:'rgba(0,0,0,0.3)', fontSize:16}}> 3</Text>
+                  <Text style={{color:'rgba(0,0,0,0.3)', fontSize:16}}> 201</Text>
                 </Col>
                 <Col size={24} style={{flexDirection:'row'}}>
                   <Icon name="ios-repeat"/>
-                  <Text style={{color:'rgba(0,0,0,0.3)', fontSize:16}}> 9</Text>
+                  <Text style={{color:'rgba(0,0,0,0.3)', fontSize:16}}> 1068</Text>
                 </Col>
                 <Col size={24} style={{flexDirection:'row'}}>
                   <Icon name="ios-heart-outline"/>
-                  <Text style={{color:'rgba(0,0,0,0.3)', fontSize:16}}> 16</Text>
+                  <Text style={{color:'rgba(0,0,0,0.3)', fontSize:16}}> 1729</Text>
                 </Col>
                 <Col size={24} style={{flexDirection:'row'}}>
                   <Icon name="ios-mail-outline"/>
@@ -104,15 +104,3 @@ class Timeline extends React.Component {
     );
   }
 }
-
-/*const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 15,
-  },
-});*/
-
-export default Timeline;
